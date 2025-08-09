fba885d359d1e08c9d421503435e9788e7c5995a
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-08-09 14:01:04 +0600

    showing real time countdown effect for expire days left

--- src/pages/home/ExpireItems.jsx
+++ src/pages/home/ExpireItems.jsx
@@ -1,44 +1,49 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import loading_chicken from "../../assets/lotti-animation/loading_chicken.json";
 import Lottie from "lottie-react";
 import { useNavigate } from "react-router";
 import axios from "axios";
-import CountUp from "react-countup";
+import Countdown from "react-countdown";
 
 const ExpireItems = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  const currnDate = new Date();
-  const formattedDate = currnDate.toISOString().split("T")[0];
-  const currentDate = formattedDate;
 
   useEffect(() => {
     axios
-      .get("https://chicken-hub-server-side-public.onrender.com/nearly-expired-foods")
+      .get(
+        "https://chicken-hub-server-side-public.onrender.com/nearly-expired-foods"
+      )
       .then((res) => {
         setData(res.data);
         setLoading(false);
       });
   }, []);
 
-  const calculateDaysLeft = (expireDate, currentDate) => {
-    const expire = new Date(expireDate);
-    const current = new Date(currentDate || new Date());
-    const diffTime = expire - current;
-    return Math.max(0, Math.ceil(diffTime / (1000 * 24 * 60 * 60)));
-  };
-
   if (loading) {
     return (
-      <div className="flex justify-center min-h-screen items-center ">
-        <Lottie animationData={loading_chicken} className=" w-24 lg:w-30" />
+      <div className="flex justify-center min-h-screen items-center">
+        <Lottie animationData={loading_chicken} className="w-24 lg:w-30" />
       </div>
     );
   }
 
   const handleDetails = (id) => {
     navigate(`/all-foods/${id}`);
+  };
+
+  // Countdown renderer
+  const renderer = ({ days, hours, minutes, seconds }) => {
+    return (
+      <p className="text-gray-600 font-medium">
+        ⏳ Expire after :{" "}
+        <span className="text-primary">
+          {" "}
+          {days !== 0 ? `${days}day` : <></>} {hours}h {minutes}m {seconds}s
+        </span>
+      </p>
+    );
   };
 
   return (
@@ -54,33 +59,26 @@ const ExpireItems = () => {
         </div>
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-7 lg:grid-cols-3">
           {data.map((food) => {
-            const daysLeft = calculateDaysLeft(food.expireDate, currentDate);
-            const initialTime = daysLeft * 24 * 60 * 60;
-
-            const hours = Math.floor(initialTime / 3600);
-            const minutes = Math.floor((initialTime % 3600) / 60);
-            const seconds = initialTime % 60;
-
             return (
               <div
                 key={food._id}
-                className="flex flex-col items-center  border border-[#bfccc0] rounded-md overflow-hidden py-4 relative"
+                className="flex flex-col items-center border border-[#bfccc0] rounded-md overflow-hidden py-4 relative"
               >
-                <div className="bg-green-600  rounded-md text-white  text-[10px]  py-1 px-2 absolute  top-3 right-3">
+                <div className="bg-green-600 rounded-md text-white text-[10px] py-1 px-2 absolute top-3 right-3">
                   In Stock
                 </div>
-                <div className="border-[#bfccc0] border text-primary backdrop-filter rounded-md  text-sm  py-1 px-1 absolute top-3 left-3">
-                  <span className="text-gray-600">⏳Expire after : </span>
-                  {String(hours).padStart(2, "0")}:
-                  {String(minutes).padStart(2, "0")}:
-                  {String(seconds).padStart(2, "0")}
-                  <p></p>
+                <div className="border-secondary border text-primary backdrop-filter rounded-md text-sm py-1 px-1 absolute top-3 left-3">
+                  {/* Countdown Animation */}
+                  <Countdown
+                    date={new Date(food.expireDate).getTime()}
+                    renderer={renderer}
+                  />
                 </div>
                 <div className="mb-4">
                   <img
                     src={food.foodPhoto}
                     alt="Seller-Img"
-                    className=" w-40 h-40 lg:w-60 lg:h-60 object-cover rounded-md"
+                    className="w-40 h-40 lg:w-60 lg:h-60 object-cover rounded-md"
                   />
                 </div>
                 <div className="text-center border-t border-[#bfccc0] border-dashed w-full">
@@ -88,16 +86,16 @@ const ExpireItems = () => {
                     {food.foodName}
                   </h3>
                   <p className="text-[#999999] text-lg font-normal">
-                    Expire Date :{" "}
-                    <span className="text-[#212529]"> {food.expireDate}</span>{" "}
+                    Expire Date:{" "}
+                    <span className="text-[#212529]">{food.expireDate}</span>
                   </p>
                   <p className="text-[#999999] text-lg font-normal">
-                    Category :{" "}
-                    <span className="text-[#212529]"> {food.category}</span>{" "}
+                    Category:{" "}
+                    <span className="text-[#212529]">{food.category}</span>
                   </p>
                   <p className="text-[#999999] text-lg font-normal">
-                    Quantity :{" "}
-                    <span className="text-[#212529]"> {food.quantity}</span>{" "}
+                    Quantity:{" "}
+                    <span className="text-[#212529]">{food.quantity}</span>
                   </p>
                   <div className="flex justify-center">
                     <button
